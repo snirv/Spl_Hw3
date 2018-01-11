67000655e9424bcb4d0bce1a3b96227d312a53d9
sharon <sharon.daniely7@gmail.com>
2018-01-11 16:31:15 +0200

    including java doc for connectonImpl

--- src/main/java/bgu/spl181/net/api/bidi/ConnectionsImpl.java
+++ src/main/java/bgu/spl181/net/api/bidi/ConnectionsImpl.java
@@ -6,6 +6,12 @@ import java.sql.Connection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * this class implements {@link Connections, and allow the system to controll the sending massege
+ *
+ * this class has clients map the holds all the connectionHandlers for the clients
+ *
+ */
 public class ConnectionsImpl<T> implements Connections<T> {
 
     protected ConcurrentHashMap<Integer,ConnectionHandler<T>> clients;
@@ -14,6 +20,12 @@ public class ConnectionsImpl<T> implements Connections<T> {
         this.clients = new ConcurrentHashMap<>();
     }
 
+    /**
+     * send to te connecion id the msg
+     * @param connectionId the connection Id that sould sen him a massege
+     * @param msg the message that send
+     * @return
+     */
     @Override
     public boolean send(int connectionId, T msg) {
         try {
@@ -24,14 +36,22 @@ public class ConnectionsImpl<T> implements Connections<T> {
         }
     }
 
+    /***
+     * broadcast to all clients a messae
+     * @param msg
+     */
     @Override
-    public void broadcast(T msg) {//TODO need to verify if the connection is active
+    public void broadcast(T msg) {
         for (Map.Entry<Integer,ConnectionHandler<T>> entry : clients.entrySet()){
             send(entry.getKey(),msg);
         }
 
     }
 
+    /**
+     * disconnect the clients by removing him from the clients' map
+     * @param connectionId that should disconnect
+     */
     @Override
     public void disconnect(int connectionId) {
         clients.remove(connectionId);
